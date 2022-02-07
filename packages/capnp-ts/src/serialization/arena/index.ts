a4bca85f5fd2a956d60cd275af839ef9c68a4f81
Sahir Gomez <84393628+sahirgomez1@users.noreply.github.com>
2022-02-06 22:59:38 -0500

    Update index.ts
    requires using 'export type'

--- packages/capnp-ts/src/serialization/arena/index.ts
+++ packages/capnp-ts/src/serialization/arena/index.ts
@@ -2,7 +2,7 @@
  * @author jdiaz5513
  */
 
-export { AnyArena } from "./any-arena";
+export type { AnyArena } from "./any-arena";
 export { Arena } from "./arena";
 export { ArenaKind } from "./arena-kind";
 export { MultiSegmentArena } from "./multi-segment-arena";
